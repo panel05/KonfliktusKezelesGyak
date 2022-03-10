98ae74ece561b14427b6148ca298fd1e20e849ae
P-Andris <pillerandras0714@gmail.com>
2022-03-10 13:06:28 +0100

    Első név inicializálása

--- src/csapatmunka_20220310/Csapatmunka_20220310.java
+++ src/csapatmunka_20220310/Csapatmunka_20220310.java
@@ -3,7 +3,7 @@ package csapatmunka_20220310;
 public class Csapatmunka_20220310 {
 
     public static void main(String[] args) {
-        String nev1, nev2;
+        String nev1 = "Piller András", nev2;
     }
     
 }
